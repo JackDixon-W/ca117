268302fcae856d7ca80f4e44a3852671cd29c528
JackDixon-W <jdixon.dixon04@gmail.com>
2024-01-22 09:50:25 +0000

    Lab 2 Finished

--- W1/1.2/league_012.py
+++ W1/1.2/league_012.py
@@ -7,21 +7,24 @@ inp = sys.stdin.readlines()
 length = 0
 for line in inp:
   line = line.rstrip("\n")
-  if len(line) > length:
-    length = len(line)
+  info = line.split()
+  club_end = len(info) - 8
+  club_name = " ".join(info[1:club_end])
+  if len(club_name) > length:
+    length = len(club_name)
 
-# Total of 35 characters + CLUB (Includes whitespace between columns)
-club = length - 35
-print(club)
+temp = "CLUB"
+print(f'POS {temp:<{length}} ', end = '')
+print(f' P   W   D   L  GF  GA  GD PTS')
 
 for line in inp:
   line = line.rstrip("\n")
   info = line.split()
   club_end = len(info) - 8
   club_name = " ".join(info[1:club_end])
-  print(f'{info[0]:>3} {club_name:<{club}} ', end = '')
-  print(f'{info[club_end + 1]:>2} {info[club_end + 2]:>3} ', end = '')
-  print(f'{info[club_end + 3]:>3} {info[club_end + 4]:>3} ', end = '')
-  print(f'{info[club_end + 5]:>3} {info[club_end + 6]:>3} ', end = '')
-  print(f'{info[club_end + 7]:>3}')
+  print(f'{info[0]:>3} {club_name:<{length}} ', end = '')
+  print(f'{info[club_end]:>2} {info[club_end + 1]:>3} ', end = '')
+  print(f'{info[club_end + 2]:>3} {info[club_end + 3]:>3} ', end = '')
+  print(f'{info[club_end + 4]:>3} {info[club_end + 5]:>3} ', end = '')
+  print(f'{info[club_end + 6]:>3} {info[club_end + 7]:>3}')
 
