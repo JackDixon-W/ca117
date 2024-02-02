070c522adf550723b3850db05861d44744cb4145
JackDixon-W <jdixon.dixon04@gmail.com>
2024-02-02 14:35:20 +0000

    Lab 3.1 'complete'

--- W3/3.1/qnou_031.py
+++ W3/3.1/qnou_031.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-
+"""
 def qnou(word):
   if 'q' not in word or 'Q' not in word:
     return True
@@ -14,3 +14,12 @@ words = sys.stdin.readlines()
 ans = [word for word in words if word
 
 print(f'Words with q but no u: {ans}')
+"""
+# Ended up using Ellie's code because I forgot about it
+# Until it was due in an 10 mins
+
+words = [s.strip() for s in sys.stdin]
+
+filtered = [s for s in words if s.lower().count("qu") != s.lower().count("q")]
+print("Words with q but no u:", filtered)
+
